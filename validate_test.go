013cb3f6e5a6a3d174fdd6ca1a8155b5705364ac
Matheus Alves <theuves@gmail.com>
2020-10-04 12:11:17 -0400

    Add TestGetAllDigits

--- validate_test.go
+++ validate_test.go
@@ -6,14 +6,30 @@ import (
 )
 
 func TestValidate(t *testing.T) {
-	var check_cpf bool
+	var checkCpf bool
 
-  check_cpf = Validate("111.444.777-35")
-	assert.Equal(t, true, check_cpf, "should be 'true'")
+  checkCpf = Validate("111.444.777-35")
+	assert.Equal(t, true, checkCpf, "should be 'true'")
 
-	check_cpf = Validate("218.425.985-38")
-	assert.Equal(t, true, check_cpf, "should be 'true'")
+	checkCpf = Validate("218.425.985-38")
+	assert.Equal(t, true, checkCpf, "should be 'true'")
 
-	check_cpf = Validate("218.425.985-42")
-	assert.Equal(t, false, check_cpf, "should be 'false'")
+	checkCpf = Validate("218.425.985-42")
+	assert.Equal(t, false, checkCpf, "should be 'false'")
+}
+
+func TestParseCpf(t *testing.T) {
+	cpfNumber, _ := parseCpf("111.444.777-35")
+	expectedDigits := [9]int{1, 1, 1, 4, 4, 4, 7, 7, 7}
+	expectedCheckDigits := [2]int{3, 5}
+
+	assert.Equal(t, expectedDigits, cpfNumber.digits, "should be CPF digits")
+	assert.Equal(t, expectedCheckDigits, cpfNumber.check_digits, "should be CPF check digits")
+}
+
+func TestGetAllDigits(t *testing.T) {
+	param := getAllDigits("111.444.777-35")
+	expected := []int{1, 1, 1, 4, 4, 4, 7, 7, 7, 3, 5}
+
+	assert.Equal(t, expected, param, "should get all digits")
 }
